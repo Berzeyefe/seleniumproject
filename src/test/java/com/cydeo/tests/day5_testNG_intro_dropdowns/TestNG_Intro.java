a97230f87bc335a53e3c71c853f643ab87141b4f
Berzeyefe <erolaksel1@gmail.com>
2022-05-23 14:06:06 +0300

    testing testNG Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -7,41 +7,47 @@ public class TestNG_Intro {
 
     @Test(priority = 1)
     public void test1() {
+        System.out.println("Test 1 is running...");
 
+        //ASSERT EQUALS: compare 2 of the same things
+        String actual = "apple";
+        String expected = "apple";
 
-        @Test(priority = 2)
-        public void test2 () {
-            System.out.println("Test 2 is running...");
+        Assert.assertEquals(actual, expected);
 
-            //AssertTrue
-            String actual = "apple";
-            String expected = "apple";
+    }
 
-            Assert.assertTrue(actual.equals(expected), "your message will go here");
+    @Test(priority = 2)
+    public void test2() {
+        System.out.println("Test 2 is running...");
 
-        }
+        //AssertTrue
+        String actual = "apple";
+        String expected = "apple";
 
-        @BeforeClass
-        public void setupMethod () {
-            System.out.println("-----> BeforeClass is running!");
+        Assert.assertTrue(actual.equals(expected), "your message will go here");
 
-        }
+    }
 
-        @AfterClass
-        public void tearDownClass () {
-            System.out.println("-----> AfterClass is running!");
+    @BeforeClass
+    public void setupMethod() {
+        System.out.println("-----> BeforeClass is running!");
 
-        }
+    }
 
-        @BeforeMethod
-        public void setUpMethod () {
-            System.out.println("---> BeforeMethod is running!");
-        }
+    @AfterClass
+    public void tearDownClass() {
+        System.out.println("-----> AfterClass is running!");
 
-        @AfterMethod
-        public void tearDownMethod () {
-            System.out.println("---> AfterMethod is running!");
-        }
+    }
 
+    @BeforeMethod
+    public void setUpMethod() {
+        System.out.println("---> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        System.out.println("---> AfterMethod is running!");
     }
 }
