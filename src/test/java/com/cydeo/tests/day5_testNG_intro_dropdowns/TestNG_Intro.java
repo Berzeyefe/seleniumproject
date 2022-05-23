d42af726dfcea39a02ba11f88509d72ad8a6b2d1
Berzeyefe <erolaksel1@gmail.com>
2022-05-23 13:42:01 +0300

    testing testNG Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -6,16 +6,8 @@ import org.testng.annotations.*;
 public class TestNG_Intro {
 
     @Test (priority = 1)
-    public void test1(){
-        System.out.println("Test 1 is running...");
+    public void test1()
 
-        //ASSERT EQUALS: compare 2 of the same things
-        String actual = "apple";
-        String expected = "apple";
-
-        Assert.assertEquals(actual, expected);
-
-    }
 
     @Test (priority = 2)
     public void test2(){
