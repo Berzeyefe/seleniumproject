9b08df5e80a9a98cd524b0db7eb64ac095de5220
Berzeyefe <erolaksel1@gmail.com>
2022-05-31 19:19:49 +0300

    day9_javaFakerPractice

---
+++ src/test/java/com/cydeo/tests/day9_javapaker_testbase_driverUtil/JavaFakerPractice.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day9_javapaker_testbase_driverUtil;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
+public class JavaFakerPractice {
+
+    @Test
+    public void Test1(){
+  // Creating Faker object to reach methods
+    Faker faker = new Faker();
+
+        System.out.println("faker.name().firstName() = " + faker.name().firstName());
+    }
+
+}
